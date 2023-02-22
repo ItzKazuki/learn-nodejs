f3881c485608b3c753e3f3aa87211523cac0a18d
Kazuki <ibnu235729@gmail.com>
2023-02-22 14:16:25 +0700

    menghapus .env karna tidak berguna dan menambah beberapa config

---
+++ config/app.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    NAME: process.env.APP_NAME,
+    APP_ENV: process.env.APP_ENV,
+    LOCATION: process.env.APP_LOCATION,
+    PORT: process.env.APP_PORT
+};
---
+++ config/mail.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    ADDRESS: process.env.EMAIL_ADDRESS,
+    USERNAME: process.env.EMAIL_USERNAME,
+    PASSWORD: process.env.EMAIL_PASSWORD,
+    PORT: process.env.MAIL_PORT,
+    SECURITY: process.env.MAIL_SECURITY,
+    FROM: process.env.MAIL_FROM,
+    NAME: process.env.MAIL_APP_NAME
+};
